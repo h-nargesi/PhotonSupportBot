820e6c2950c8b24e8899e2262b82aeee88147ae1
Ryan Nargesi <ryan.nargesi.ai@gmail.com>
2024-02-23 21:58:57 +0330

    after merge

--- python/extension.py
+++ python/extension.py
@@ -1,3 +1,6 @@
+import logging
+import globalvalues as gv
+
 from telebot import types
 from globalvalues import BOT, MESSAGES, USERS, VARIABLES
 
@@ -11,29 +14,29 @@ def Init():
 def ExtendUser(message):
     BOT.send_message(message.chat.id, MESSAGES["out-of-service"], parse_mode='markdown')
 
-    # logging_info = { 'userid': message.chat.id }
-    # request_text = message.text.split(" ")[1:]
+    logging_info = gv.GetLogInfo(message.chat.id)
+    request_text = message.text.split(" ")[1:]
     
-    # logging.info('extension command: (%s)', ')('.join(request_text), extra=logging_info)
+    logging.info('extension command: (%s)', ')('.join(request_text), extra=logging_info)
 
-    # if VARIABLES.ADMIN < 0:
-    #     BOT.send_message(message.chat.id, MESSAGES["global-error"], parse_mode='markdown')
-    #     return
+    if VARIABLES.ADMIN < 0:
+        BOT.send_message(message.chat.id, MESSAGES["global-error"], parse_mode='markdown')
+        return
     
-    # if len(request_text) == 0:
-    #     output = MESSAGES_EXT['get-user-name']
-    #     if message.chat.id in USERS and 'name' in USERS[message.chat.id]:
-    #         output += MESSAGES_EXT['default-user'].format(USERS[message.chat.id]['name'])
-    #     BOT.send_message(message.chat.id, output, parse_mode='markdown')
+    if len(request_text) == 0:
+        output = MESSAGES_EXT['get-user-name']
+        if message.chat.id in USERS and 'name' in USERS[message.chat.id]:
+            output += MESSAGES_EXT['default-user'].format(USERS[message.chat.id]['name'])
+        BOT.send_message(message.chat.id, output, parse_mode='markdown')
         
-    #     BOT.register_next_step_handler(message, GetUsername)
+        BOT.register_next_step_handler(message, GetUsername)
 
-    # elif len(request_text) == 1:
-    #     message.text = request_text[0]
-    #     GetUsername(message)
+    elif len(request_text) == 1:
+        message.text = request_text[0]
+        GetUsername(message)
     
-    # else:
-    #     BOT.send_message(message.chat.id, MESSAGES["invlid-request"], parse_mode='markdown')
+    else:
+        BOT.send_message(message.chat.id, MESSAGES["invlid-request"], parse_mode='markdown')
 
 def GetUsername(message):
     username = message.text if message.text != '.' else USERS[message.chat.id]['name']
