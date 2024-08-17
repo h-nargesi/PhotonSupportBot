71dec0de6ba43e4afe6ccb7175a46dab2a2f36b2
hamedna <hamedna@systemgroup.net>
2024-08-17 18:22:04 +0330

    use total_data_limit

--- python/settings/topup-info.sql
+++ python/settings/topup-info.sql
@@ -1,4 +1,4 @@
-select username, round(data / 1024 / 1024 / 1024, 2) as data, time, days_to_use, type
+select username, to_gigabyte(data, 2) as data, time, days_to_use, type
 from (
 	select u.username, t.data, t.time, t.days_to_use, t.type
 		, row_number() over(partition by t.permanent_user_id order by t.id desc) as ranking
--- python/settings/user-info.sql
+++ python/settings/user-info.sql
@@ -1,15 +1,15 @@
 select username
 	, floor(time_left / 24) as left_days
 	, mod(time_left, 24) as left_hours
-	, round(data_left / 1024 / 1024 / 1024, 2) as giga_left
+	, to_gigabyte(data_left, 2) as giga_left
 	, time_left as total_hour_left
 from (
 	select *
 		, greatest(0, timestampdiff(hour, now(), expiration)) as time_left
-		, total_data - ifnull(data_usage, 0) as data_left
+		, total_data_limit - ifnull(data_usage, 0) as data_left
 	from (
 		select u.username, u.expiration
-			, case when u.reset_type_data is null then null else ifnull(u.total_data, 0) end as total_data
+			, case when u.reset_type_data is null then null else ifnull(u.total_data_limit, 0) end as total_data_limit
 			, case when u.reset_type_data is null then null else (
 				select sum(acctinputoctets) + sum(acctoutputoctets) as data_usage
 				from radacct d
