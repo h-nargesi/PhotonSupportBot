475c58003b8850b4aed4e36f6011e8eab9891e7e
Ryan Nargesi <hamed.nargesi.jar@gmail.com>
2024-10-18 01:36:00 +0330

    admin logs

--- python/approvement.py
+++ python/approvement.py
@@ -49,12 +49,15 @@ def callback_query_handler(call):
 @BOT.message_handler(commands=["admin"])
 def RegisterAdmin(message):
     request_text = message.text.split(" ")[1:]
+    logging_info = gv.GetLogInfo(message.chat.id)
 
     if len(request_text) != 1 or request_text[0] != TOKEN.split(":")[1]:
+        log.critical('[admin]: unsucessfull try to set admin', **logging_info)
         BOT.send_message(message.chat.id, MESSAGES_APV["invalid-pass"], parse_mode='markdown')
         return
 
     VARIABLES.ADMIN = message.chat.id
+    log.critical('[admin]: has been set', **logging_info)
     BOT.send_message(message.chat.id, MESSAGES_APV["set"], parse_mode='markdown')
 
 def ApprovePayment(user_id):
